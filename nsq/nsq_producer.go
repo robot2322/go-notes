4e9101ef44fc5e82524a72f7c090483fd68e5986
robot2322 <zhangyaoyin@jinka.cn>
2022-06-08 16:50:10 +0800

    nsq

---
+++ nsq/nsq_producer.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"github.com/nsqio/go-nsq"
+	"os"
+	"strings"
+)
+
+var producer *nsq.Producer
+
+func initProducer(str string) (err error) {
+	config := nsq.NewConfig()
+	producer, err = nsq.NewProducer(str, config)
+	if err != nil {
+		fmt.Printf("create producer failed, err:%v\n", err)
+		return err
+	}
+	return nil
+}
+
+func main() {
+	nsqAddress := "127.0.0.1:4150"
+	err := initProducer(nsqAddress)
+	if err != nil {
+		fmt.Printf("init producer failed, err:%v\n", err)
+		return
+	}
+
+	reader := bufio.NewReader(os.Stdin) // 读取标准输入
+	for {
+		data, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Printf("read string from stdin failed, err:%v\n", err)
+			continue
+		}
+		data = strings.TrimSpace(data)
+		if strings.ToUpper(data) == "Q" { // 输入q退出
+			break
+		}
+
+		err = producer.Publish("topic_demo", []byte(data))
+		if err != nil {
+			fmt.Printf("publish msg to nsq failed, err:%v\n", err)
+			continue
+		}
+
+	}
+
+}
